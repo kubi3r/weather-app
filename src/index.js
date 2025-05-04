63479e990f4c05210f2d9675bad19deb6ea6df99
kubier <opisnerf@gmail.com>
2025-05-05 01:53:38 +0300

    Add form

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import './styles.css';
 import getWeather from './modules/getWeather.js';
 import parseWeather from './modules/parseWeather.js';
 
-getWeather('amsterdam').then((weather) => {
-	console.log(weather);
-	console.log(parseWeather(weather));
+const form = document.querySelector('form');
+const locationInput = document.querySelector('input[id="location"]');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	getWeather(locationInput.value).then((weather) => {
+		console.log(weather);
+		console.log(parseWeather(weather));
+	});
 });
